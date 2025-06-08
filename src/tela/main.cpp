9fee37df5293a8d20a5de0330683d1bb44224d82
Giulia <giulia.souza.leite.04@gmail.com>
2025-06-08 17:17:47 -0300

    marco 3 teste

--- src/tela/main.cpp
+++ src/tela/main.cpp
@@ -11,101 +11,38 @@
 #include "respostas.json"
 #include <ArduinoJson.h>
 
-#define BUZZER_PIN 22
+#define BUZZER_PIN 13
 #define BUTTON_PIN 36
+#define BUTTON_EMERGENCY 39
+#define BUZZER_CHANNEL 6
 static const int servoPin = 5;
 
 Servo servo1;
 
 AsyncWebServer server(80);
-const char* ssid = "Redmi Note 14";
-const char* password = "giugiu24";
+const char* ssid = "Morena branca";
+const char* password = "jujuba25";
 TFT_eSPI tft = TFT_eSPI();
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Pomodoro com ESP32</title>
-  </head>
-  <body>
-    <h1>Pomodoro em andamento...</h1>
-    <p id="status">Status: Aguardando início...</p>
-
-    <form onsubmit="enviarConfiguracao(); return false;">
-      <label>Tempo de trabalho: <input type="number" id="foco" value="1500"></label><br>
-      <label>Tempo de pausa: <input type="number" id="pausa" value="300"></label><br>
-      <button type="submit">Atualizar Ciclos</button>
-    </form>
-
-    <div id="pesquisa" style="display:none;">
-      <p id="pergunta"></p>
-      <input type="text" id="resposta">
-      <button onclick="enviarResposta()">Enviar</button>
-    </div>
-
-    <script>
-      function enviarConfiguracao() {
-        const foco = document.getElementById("foco").value;
-        const pausa = document.getElementById("pausa").value;
-
-        fetch("/config", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: `foco=${foco}&pausa=${pausa}`
-        }).then(res => {
-          if (res.ok) alert("Ciclos atualizados!");
-        });
-      }
-
-      function exibirPergunta(pergunta) {
-        document.getElementById("pergunta").innerText = pergunta;
-        document.getElementById("pesquisa").style.display = "block";
-      }
-
-      function enviarResposta() {
-        const resp = document.getElementById("resposta").value;
-        fetch("/resposta", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: `resposta=${resp}`
-        }).then(res => {
-          if (res.ok) {
-            document.getElementById("pesquisa").style.display = "none";
-          }
-        });
-      }
-
-      setInterval(() => {
-        fetch("/status")
-          .then(res => res.json())
-          .then(data => {
-            document.getElementById("status").innerHTML = data.emTrabalho ? "Status: Trabalho" : "Status: Pausa";
-            if (data.pergunta) {
-              exibirPergunta(data.pergunta);
-            }
-          });
-      }, 2000);
-    </script>
-  </body>
-</html>
-)rawliteral";
-
 uint32_t lastSecond = 0;
-int duracaoFoco = 5;
-int duracaoPausa = 5;
+int duracaoFoco = 25 * 60;
+int duracaoPausa = 5 * 60;
 int tempoRestante = duracaoFoco;
+int num_ciclos = 1;
+int contEmergency = 0;
+
 bool emTrabalho = true;
 bool somTocado = false;
 bool cicloFinalizado = false;
 bool pomodoroIniciado = false;
 bool esperandoResposta = false;
-int num_ciclos = 1;
-String perguntaAtual = "";
+bool iniciarPomodoro_aux = false;
 bool girou = false;
-//bool girouFim = false; // Variável para controlar o giro do servo no fim
 
-int servoPos = 0; // posição atual do servo
+static bool lastEmergencyState = HIGH;
+unsigned long lastEmergencyClick = 0;
+
+String perguntaAtual = "";
 
 void salvarResposta(const String& pergunta, const String& resposta) {
   JsonDocument doc;
@@ -120,11 +57,11 @@ void salvarResposta(const String& pergunta, const String& resposta) {
 }
 
 void playTone(int freq, int dur) {
-  ledcSetup(0, freq, 8);
-  ledcAttachPin(BUZZER_PIN, 0);
-  ledcWrite(0, 128);
+  ledcSetup(BUZZER_CHANNEL, freq, 8);
+  ledcAttachPin(BUZZER_PIN, BUZZER_CHANNEL);
+  ledcWrite(BUZZER_CHANNEL, 128);
   delay(dur);
-  ledcWrite(0, 0);
+  ledcWrite(BUZZER_CHANNEL, 0);
 }
 
 void playWorkEndTone() {
@@ -137,158 +74,112 @@ void playBreakEndTone() {
   playTone(500, 500);
 }
 
+void playEmergencyTone() {
+  playTone(2000, 500); delay(100);
+  playTone(2000, 500); delay(100);
+  playTone(2000, 500);
+}
+
 void mostrarPerguntaTFT(const String& pergunta) {
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_YELLOW);
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextColor(TFT_YELLOW, TFT_WHITE);
   tft.setTextSize(2);
-  tft.setCursor(10, 50);
+  int16_t x = (tft.width() - tft.textWidth((char*)pergunta.c_str())) / 2;
+  tft.setCursor(x, 50);
   tft.println(pergunta);
 }
 
 void atualizarTela() {
   if (esperandoResposta) return;
   uint16_t bgColor = emTrabalho ? TFT_GREEN : TFT_SKYBLUE;
-
+  tft.fillScreen(TFT_WHITE);
   tft.setTextSize(2);
-  tft.setCursor(20, 50);
 
   if (!pomodoroIniciado) {
-    tft.setTextColor(TFT_YELLOW, TFT_BLACK);
-    tft.print("Pressione o botao");
-    tft.setCursor(20, 80);
-    tft.print("para iniciar!");
+    tft.setTextColor(TFT_RED, TFT_WHITE);
+    int16_t x1 = (tft.width() - tft.textWidth("Pressione o botao")) / 2;
+    int16_t x2 = (tft.width() - tft.textWidth("para iniciar!")) / 2;
+    tft.setCursor(x1, 50); tft.print("Pressione o botao");
+    tft.setCursor(x2, 80); tft.print("para iniciar!");
   } else {
-    tft.setTextColor(bgColor, TFT_BLACK);
-    tft.setCursor(20, 50);
-    tft.print(emTrabalho ? "Trabalhando..." : "Pausa...");
+    tft.setTextColor(bgColor, TFT_WHITE);
+    String status = emTrabalho ? "Trabalhando..." : "Pausa...";
+    int16_t xStatus = (tft.width() - tft.textWidth((char*)status.c_str())) / 2;
+    tft.setCursor(xStatus, 50);
+    tft.print(status);
 
     int minutos = tempoRestante / 60;
     int segundos = tempoRestante % 60;
     tft.setTextSize(6);
-    tft.setCursor(60, 100);
-    if (minutos < 10) tft.print("0");
-    tft.print(minutos);
-    tft.print(":");
-    if (segundos < 10) tft.print("0");
-    tft.print(segundos);
+    char tempoStr[6];
+    sprintf(tempoStr, "%02d:%02d", minutos, segundos);
+    int16_t xTempo = (tft.width() - tft.textWidth(tempoStr)) / 2;
+    tft.setCursor(xTempo, 100);
+    tft.print(tempoStr);
   }
 }
 
-// void girarServoInicio() {
-//   if (servoPos == 0) {
-//     for (int posDegrees = 0; posDegrees <= 10; posDegrees++) {
-//       servo1.(1);
-//       delay(15);
-//     }
-//     servoPos = 180;
-//   }
-// }
+void girarServoInicio() {
+  for (int pos = 0; pos <= 180; pos++) {
+    servo1.write(pos);
+    delay(15);
+  }
+}
 
-// void girarServoFim() {
-//   if (servoPos == 180) {
-//     for (int posDegrees = 10; posDegrees >= 0; posDegrees--) {
-//       servo1.write(-1);
-//       delay(15);
-//     }
-//     servoPos = 0;
-//   }
-// }
+void girarServoFim() {
+  for (int pos = 180; pos >= 0; pos--) {
+    servo1.write(pos);
+    delay(15);
+  }
+}
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(BUZZER_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT);
-  servo1.attach(servoPin);
-  SPIFFS.begin(true);
-
-  tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
-  tft.setTextSize(2);
-  tft.setCursor(10, 10);
-  tft.print("Pomodoro configuravel");
-  delay(1000);
-
+void emergencyStop() {
+  Serial.println("Parando o pomodoro de emergencia!");
+  pomodoroIniciado = false;
+  emTrabalho = true;
+  tempoRestante = duracaoFoco;
+  somTocado = false;
+  cicloFinalizado = false;
+  esperandoResposta = false;
   atualizarTela();
-
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi conectado");
-  Serial.println(WiFi.localIP());
-
-  String ipStr = "IP: " + WiFi.localIP().toString();
-  tft.setTextSize(2);
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  tft.setCursor(240 - tft.textWidth((char*)ipStr.c_str()) - 5, 230);
-  tft.print(ipStr);
-
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *req){
-    req->send_P(200, "text/html", index_html);
-  });
-
-  server.on("/status", HTTP_GET, [](AsyncWebServerRequest *req){
-    String json = String("{\"fim\":") + (cicloFinalizado ? "true" : "false") +
-                  String(",\"emTrabalho\":") + (emTrabalho ? "true" : "false") +
-                  String(",\"iniciado\":") + (pomodoroIniciado ? "true" : "false") +
-                  String(",\"pergunta\":\"") + (esperandoResposta ? perguntaAtual : "") + String("\"}");
-    req->send(200, "application/json", json);
-  });
-
-  server.on("/config", HTTP_POST, [](AsyncWebServerRequest *req){
-    if (req->hasParam("foco", true) && req->hasParam("pausa", true)) {
-      duracaoFoco = req->getParam("foco", true)->value().toInt()*60;
-      duracaoPausa = req->getParam("pausa", true)->value().toInt()*60;
-      tempoRestante = emTrabalho ? duracaoFoco : duracaoPausa;
-      req->send(200, "text/plain", "Ciclos atualizados");
-    } else req->send(400, "text/plain", "Parâmetros inválidos");
-  });
-
-  server.on("/resposta", HTTP_POST, [](AsyncWebServerRequest *req){
-    if (req->hasParam("resposta", true)) {
-      String resposta = req->getParam("resposta", true)->value();
-      salvarResposta(perguntaAtual, resposta);
-      esperandoResposta = false;
-      req->send(200, "text/plain", "Recebido");
-    }
-  });
-
-  server.begin();
 }
 
-bool teste = false;
+void screenEmergency() {
+  tft.fillScreen(TFT_RED);
+  tft.setTextColor(TFT_WHITE, TFT_RED);
+  tft.setTextSize(4);
+  tft.setCursor(20, 100);
+  tft.print("BOTAO DE EMERGENCIA ACIONADO!");
+  playEmergencyTone();
+  delay(200);
+  emergencyStop();
+}
 
-void loop() {
+void pomodoroBotaoIniciar() {
   if (!pomodoroIniciado && digitalRead(BUTTON_PIN) == HIGH) {
     delay(200);
-    perguntaAtual = "Como você se sente para estudar?";
+    perguntaAtual = "Como voce se sente para estudar?";
     esperandoResposta = true;
-    //mostrarPerguntaTFT(perguntaAtual);
-    teste = true;
+    iniciarPomodoro_aux = true;
   }
+}
 
-  if (esperandoResposta) return;
-
-  if (!pomodoroIniciado && teste) {
+void pomodoroIniciar() {
+  if (!pomodoroIniciado && iniciarPomodoro_aux) {
+    girarServoInicio();
     playWorkEndTone();
-    tft.fillScreen(TFT_BLACK);
-    tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+    tft.fillScreen(TFT_WHITE);
+    tft.setTextColor(TFT_BLACK, TFT_WHITE);
     tft.setTextSize(2);
     tft.setCursor(10, 10);
     tft.print("Pomodoro configuravel");
-    //if(girouFim==false){
-      //Serial.println("Servo girado para 180 graus");
-      //girouFim = true;
-    //}
     pomodoroIniciado = true;
-    
-    teste = false;
+    iniciarPomodoro_aux = false;
     lastSecond = millis();
   }
+}
 
+void pomodoroLogica() {
   if (pomodoroIniciado && millis() - lastSecond >= 1000 && num_ciclos > 0) {
     lastSecond += 1000;
     tempoRestante--;
@@ -316,31 +207,133 @@ void loop() {
     if (somTocado && tempoRestante >= 0)
       somTocado = false;
   }
+}
 
+void pomodoroFinalizar() {
   if (num_ciclos <= 0 && cicloFinalizado) {
     if (somTocado) {
       somTocado = false;
       cicloFinalizado = true;
       playWorkEndTone();
-      perguntaAtual = "Como você se sente apos estudar?";
+      perguntaAtual = "Como voce se sente apos estudar?";
       esperandoResposta = true;
-      //mostrarPerguntaTFT(perguntaAtual);
     }
 
     if (!esperandoResposta) {
       cicloFinalizado = false;
-      tft.fillScreen(TFT_BLACK);
-      tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+      tft.fillScreen(TFT_WHITE);
+      tft.setTextColor(TFT_YELLOW, TFT_WHITE);
       tft.setTextSize(6);
-      tft.setCursor(10, 10);
+      int16_t xFim = (tft.width() - tft.textWidth("FIM!")) / 2;
+      tft.setCursor(xFim, 10);
       tft.print("FIM!");
-      if(girou==false){
-        // servo1.write(0);
-        // delay(200);
-        //servo1.write(180);
-        //delay(1000); 
-        //girou = true;
+      girarServoFim();
+    }
+  }
+}
+
+void pomodoroEmergencia() {
+  bool currentState = digitalRead(BUTTON_EMERGENCY);
+  unsigned long now = millis();
+
+  if (lastEmergencyState == HIGH && currentState == LOW) {
+    if (now - lastEmergencyClick > 5000) {
+      contEmergency = 0; // Reset se passou mais de 5 segundos entre cliques
+    }
+
+    if (now - lastEmergencyClick > 300) { // Debounce de 300ms
+      contEmergency++;
+      Serial.print("Emergencia cliques: "); Serial.println(contEmergency);
+      lastEmergencyClick = now;
+
+      if (contEmergency >= 10) {
+        screenEmergency();
+        contEmergency = 0;
       }
     }
   }
+
+  lastEmergencyState = currentState;
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_EMERGENCY, INPUT_PULLUP);
+  servo1.attach(servoPin);
+
+  if (!SPIFFS.begin(true)) {
+    Serial.println("Erro ao montar SPIFFS");
+    return;
+  }
+
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextColor(TFT_BLACK, TFT_WHITE);
+  tft.setTextSize(2);
+  tft.setCursor(10, 10);
+  tft.print("Pomodoro configuravel");
+  delay(1000);
+  atualizarTela();
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi conectado");
+  Serial.println(WiFi.localIP());
+
+  String ipStr = "IP: " + WiFi.localIP().toString();
+  tft.setTextSize(2);
+  tft.setTextColor(TFT_RED, TFT_WHITE);
+  tft.setCursor(240 - tft.textWidth((char*)ipStr.c_str()) - 5, 230);
+  tft.print(ipStr);
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *req){
+    req->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *req){
+    req->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  server.on("/status", HTTP_GET, [](AsyncWebServerRequest *req){
+    String json = String("{\"fim\":") + (cicloFinalizado ? "true" : "false") +
+                  String(",\"emTrabalho\":") + (emTrabalho ? "true" : "false") +
+                  String(",\"iniciado\":") + (pomodoroIniciado ? "true" : "false") +
+                  String(",\"pergunta\":\"") + (esperandoResposta ? perguntaAtual : "") + String("\"}");
+    req->send(200, "application/json", json);
+  });
+
+  server.on("/config", HTTP_POST, [](AsyncWebServerRequest *req){
+    if (req->hasParam("foco", true) && req->hasParam("pausa", true)) {
+      duracaoFoco = req->getParam("foco", true)->value().toInt() * 60;
+      duracaoPausa = req->getParam("pausa", true)->value().toInt() * 60;
+      tempoRestante = emTrabalho ? duracaoFoco : duracaoPausa;
+      req->send(200, "text/plain", "Ciclos atualizados");
+    } else req->send(400, "text/plain", "Parâmetros inválidos");
+  });
+
+  server.on("/resposta", HTTP_POST, [](AsyncWebServerRequest *req){
+    if (req->hasParam("resposta", true)) {
+      String resposta = req->getParam("resposta", true)->value();
+      salvarResposta(perguntaAtual, resposta);
+      esperandoResposta = false;
+      req->send(200, "text/plain", "Recebido");
+    }
+  });
+
+  server.begin();
+}
+
+void loop() {
+  pomodoroBotaoIniciar();
+  if (esperandoResposta) return;
+  pomodoroIniciar();
+  pomodoroLogica();
+  pomodoroFinalizar();
+  pomodoroEmergencia();
 }
